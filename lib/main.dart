ac6b4a00dcd3a682efde4f24cbaad8002ccab8a5
tamzidpeace <tamjedpeace@gmail.com>
2021-05-24 15:39:47 +0600

    login program done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:qr_code_scanner_example/page/qr_create_page.dart';
 import 'package:qr_code_scanner_example/page/qr_scan_page.dart';
 import 'package:qr_code_scanner_example/widget/button_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +27,9 @@ class MyApp extends StatelessWidget {
           primaryColor: Colors.blue,
           scaffoldBackgroundColor: Colors.white,
         ),
-        home: MainPage(title: title,),
+        home: MainPage(
+          title: title,
+        ),
       );
 }
 
@@ -41,73 +45,102 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) => Scaffold(
-
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-        ),
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(10,100,10, 0),
-            child: Center(
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'VQ Login',
-                        style: TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 30,
-                            decoration: TextDecoration.underline,
-                        ),
-                      )),
-                  Container(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(10, 100, 10, 0),
+          child: Center(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
                     padding: EdgeInsets.all(10),
-                    child: TextField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
+                    child: Text(
+                      'VQ Login',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30,
+                        decoration: TextDecoration.underline,
                       ),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: TextField(
-                      obscureText: true,
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                      ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
                     ),
                   ),
-                  Container(
-                      height: 50,
-                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      margin: EdgeInsets.only(top: 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ButtonWidget(
-                            text: 'Login',
-                            onClicked: () => Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) => QRScanPage(),
-                            )),
-                          ),
-                        ],
+                ),
+                Container(
+                  height: 50,
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  margin: EdgeInsets.only(top: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ButtonWidget(
+                        text: 'Login',
+                        onClicked: () => login(),
                       ),
+                    ],
                   ),
-                ],
-              ),
-            ))
+                ),
+              ],
+            ),
+          )));
+
+  Future<void> login() async {
+    var email = nameController.text;
+    var password = passwordController.text;
+
+    try {
+      var url = Uri.parse('https://user1.truhoist.com/api/employee/login');
+
+      var response = await http.post(
+        url,
+        body: {'email': email, 'password': password},
       );
+      Map data = jsonDecode(response.body);
+      print(data);
+      if (data['message'] == 'employee successfully logged in!') {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => QRScanPage(),
+        ));
+      } else {
+        _showToast(context, data['message']);
+      }
+    } catch (e) {
+      _showToast(context, 'An error occurred!');
+    }
+  }
+
+  void _showToast(BuildContext context, message) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
 }
