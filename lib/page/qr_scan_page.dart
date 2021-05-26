7ea64398abf70e2628b90a0f5d2718efcad515b7
tamzidpeace <tamjedpeace@gmail.com>
2021-05-26 16:10:03 +0600

    simple changes

--- lib/page/qr_scan_page.dart
+++ lib/page/qr_scan_page.dart
@@ -157,10 +157,8 @@ class _QRScanPageState extends State<QRScanPage> {
   }
 
   Future<Map> validateQrCode(qrCode) async {
-    var url = Uri.parse(
-        'https://user1.truhoist.com/api/qr-code/validate?id=39dsdewr&employee_id=2');
-    var token =
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNDFjOWVkNTIzNDJiYzNlOTM2MjVlNGRjNzQ0MzRjMGFlZmRhMTEwYzU3NTgyNTM2OWY0NGRiNmE3MThjZmU0ZTZiMTY0ZmRiNmYwNTBiMDkiLCJpYXQiOjE2MjE2NzI4MzcsIm5iZiI6MTYyMTY3MjgzNywiZXhwIjoxNjUzMjA4ODM3LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.kav4JgtiMfXPnR2x_XuMk8bfXW8ySEM-m0w3MWof5Get2_YQ2CjUfy_mj5eiEWlEwn3kySBDL0oF-Tv1JoNVOJ34CnViN-zEFQIETxY2qluA3CDrHJRpElJeeYFcRwgw7xvCyiLTHf4cVBtWzZvyWipHII6A6vzFU7lt-GTHBKvR9sKGhGQIntCAEkm3yNjeHwRYcdcolrfzkyFiqDnjlegmBCEFXovQdIBml2rq-13j6sPKpQvC1bQoVcU-EdB0rUmVy_oWRkGLClYwBLNmkb1SHFFRiLCc4FNpkvD2dynsPt70n2PT5Nxfp9wXSGZW7NSqsC0gOiQal-edjQSt4VNs91HAGMB-C2jlZt0HPD22EuRFGuM4hxuOzRpGV55Bqkd0nm4Ep6h9rpKA-u3P3uFGhy7zdm6lZuu-ZV_G2BeSOBFa_Gv7AjkCBREL2SC8n5qsSzhJDGZTRIoNEVpvUSOKyNBE2jzUhJm3wPAY1T7M0-Wyu2Ce953z5DnobovGy8PqROeMPx7ek0EFrTil4b2SfQwtwWFkcB7bD4V_7z3u-C9ldcMS4OyTWvXkp-8fiXpcw5G732YNBN-z3mTbNWHkMBm8-FJ-_SRkPd6c981idladHvHF5-InfvH96sNVuzONc8mV6ICpKOkHgTKfhYV9nPEuTn3UPyI6TWfW3rc';
+    var url = Uri.parse('https://user1.truhoist.com/api/qr-code/validate');
+    var token = await getStringValuesSF('token');
     var response = await http.post(
       url,
       headers: {'Authorization': 'Bearer $token'},
