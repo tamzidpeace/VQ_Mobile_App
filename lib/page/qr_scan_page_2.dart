96719da1bd95e19d47e2f0cc5b3eba2f1f393177
tamzidpeace <tamjedpeace@gmail.com>
2021-06-01 17:54:58 +0600

    its working

--- lib/page/qr_scan_page_2.dart
+++ lib/page/qr_scan_page_2.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(home: QRViewExample()));
 
@@ -15,6 +18,7 @@ class _QRViewExampleState extends State<QRViewExample> {
   Barcode result;
   QRViewController controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  String scan_qr_result = '';
 
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
@@ -33,8 +37,9 @@ class _QRViewExampleState extends State<QRViewExample> {
       body: Column(
         children: <Widget>[
           Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(flex: 1, child: Text(scan_qr_result)),
           Expanded(
-            flex: 1,
+            flex: 2,
             child: FittedBox(
               fit: BoxFit.contain,
               child: Column(
@@ -141,10 +146,19 @@ class _QRViewExampleState extends State<QRViewExample> {
     setState(() {
       this.controller = controller;
     });
-    controller.scannedDataStream.listen((scanData) {
+    controller.scannedDataStream.listen((scanData) async {
+      print(scanData.code);
+      await controller.pauseCamera();
+      var data = await validateQrCode(scanData.code);
+      print(data);
       setState(() {
-        result = scanData;
+        if (data['success'] == 'false')
+          scan_qr_result = 'Invalid Qr Code';
+        else
+          scan_qr_result = data['message'];
       });
+      //sleep(Duration(seconds: 3));
+      //await controller.resumeCamera();
     });
   }
 
@@ -152,5 +166,40 @@ class _QRViewExampleState extends State<QRViewExample> {
   void dispose() {
     controller?.dispose();
     super.dispose();
+  }
+
+  Future<Map> validateQrCode(qrCode) async {
+    var subdomain = await getStringValuesSF('subdomain');
+    var employee_id = await getIntValuesSF('employee_id');
+    var url = Uri.parse('https://' + subdomain + '/api/qr-code/validate');
+    var token = await getStringValuesSF('token');
+    var response = await http.post(
+      url,
+      headers: {'Authorization': 'Bearer $token'},
+      body: {'id': qrCode, 'employee_id': employee_id.toString()},
+    );
+    Map data = jsonDecode(response.body);
+    return data;
+  }
+
+  //add value to sf
+  addStringToSF(key, value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+  }
+
+  //get value from sf
+  Future<String> getStringValuesSF(key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Return String
+    String stringValue = prefs.getString(key);
+    return stringValue;
+  }
+
+  getIntValuesSF(key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Return String
+    var stringValue = prefs.getInt(key);
+    return stringValue;
   }
 }
